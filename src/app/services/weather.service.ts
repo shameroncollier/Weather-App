f06efd44aea34ad5e6ca744e1d39a870d922d977
shameroncollier <115970069+shameroncollier@users.noreply.github.com>
2023-08-11 08:28:57 -0500

    Initial commit

---
+++ src/app/services/weather.service.ts
@@ -0,0 +1,24 @@
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment.development';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class WeatherService {
+
+  constructor(private http: HttpClient) { 
+
+  }
+
+
+  getWeatherData(cityName: string): Observable<WeatherService> {
+    return this.http.get<WeatherService>(environment.weatherApiBaseUrl, {
+      headers: new HttpHeaders()
+      .set(environment.XRapidAPIHostHeaderName, environment.XRapidAPIHostHeaderValue)
+      .set(environment.XRapidAPIKeyHeaderName, environment.XRapidAPIKeyHeaderValue),
+      params: new HttpParams()
+      .set('city', cityName)
+    })
+  }
+}
---
+++ src/environments/environment.development.ts
@@ -0,0 +1,8 @@
+export const environment = {
+    production: false,
+    weatherApiBaseUrl: 'https://open-weather13.p.rapidapi.com/city/landon',
+    XRapidAPIHostHeaderName: 'X-RapidAPI-Host',
+    XRapidAPIHostHeaderValue: 'open-weather13.p.rapidapi.com',
+    XRapidAPIKeyHeaderName: 'X-RapidAPI-Key',
+    XRapidAPIKeyHeaderValue: '97a68159abmsh77cb800ab631dcap1ae20bjsn81d7623c3b27',
+};
